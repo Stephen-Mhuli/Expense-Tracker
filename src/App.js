b7dde76524b7f3b77c338a3d40a6ab762b590bcf
stephen-mhuli <stevemshible@gmail.com>
2022-05-02 12:27:33 +0300

    Splitting components into multiple components

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
        <ExpenseItem
        title={expenses[3].title} 
        amount={expenses[3].amount} 
-       date={expenses[3].date}
+       date={expenses[3].date} 
        />
       
       
