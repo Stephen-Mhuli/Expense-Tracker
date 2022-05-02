96895ee7d632ab8554c81c253c520eab89503418
stephen-mhuli <stevemshible@gmail.com>
2022-05-02 19:49:38 +0300

    Listening to events and working with event handlers

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ const App = () => {
   const expenses = [
     {
       id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
+      title: 'New Laptop',
+      amount: 494.12,
       date: new Date(2020, 7, 14),
     },
     { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,14 +2,22 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
+
+    let title = props.title;
+
+    const clickHandler = () => {
+        title = "Updated!";
+        console.log(title)
+    }
    
     return (
     <div className='expense-item'>
         <ExpenseDate date={props.date}/>
         <div className='expense-item__description'>
-            <h2>{ props.title }</h2>
+            <h2>{ title }</h2>
             <div className='expense-item__price'>${ props.amount }</div>
         </div>
+        <button onClick={clickHandler}>Add New Expense</button>
     </div>
     );
 }
